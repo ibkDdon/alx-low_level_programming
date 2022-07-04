8899cb00fc47d72098762d47aac8f57381e15207
ibkDdon <ibukunoluwa_aina@yahoo.com>
2022-07-04 01:08:32 +0000

    gjkokoo

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,7 @@
+/**Initial Short Description*/
 #include <stdio.h>
-/**
- * Some comment
- */
-int main(void)
+int main(void) /**Function Main Description*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return (0); /**Returns 0*/
 }

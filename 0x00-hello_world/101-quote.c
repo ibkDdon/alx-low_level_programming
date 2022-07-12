faaebbcdf77d6d71c10b7c49b11be9aed7d74afe
ibkDdon <ibukunoluwa_aina@yahoo.com>
2022-07-12 17:35:45 +0000

    gshj

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,9 @@
  * Return: return value description
  */
 int main(void)
-	char str1[ ] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
+{
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+		
 	fprintf(stderr,"%s", str1);
 	return (-1);
 }

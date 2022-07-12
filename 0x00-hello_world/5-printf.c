9174be0cbec4ec388ceccdd7fa1e9cad59b95ef5
ibkDdon <ibukunoluwa_aina@yahoo.com>
2022-07-12 16:25:20 +0000

    gshj

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+include <stdio.h>
 /**
  * main - function to Print
  * Return: Description of returned value
  */
-int main(void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);

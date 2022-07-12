de0017730eaccfa7eca7b060223931cd37e5cf87
ibkDdon <ibukunoluwa_aina@yahoo.com>
2022-07-12 16:38:12 +0000

    gshj

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio>
+/**
+ * main - main function
+ * Return: Description of return value
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}

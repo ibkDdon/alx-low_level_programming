77ed7c24bc1b628dff30036cd868ed40d7d90439
ibkDdon <ibukunoluwa_aina@yahoo.com>
2022-07-12 15:27:26 +0000

    tthh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-char str1[] = "\"Programming is like building a multilingual puzzle"; int puts(str1);
+#include <stdio.h>
+/*
+ * function test
+ */
+int main(void)
+{
+char str1[] = "\"Programming is like building a multilingual puzzle\n"; 
+int puts(str1);
+return (0);
+}

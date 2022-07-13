ad1578a52cec944eab4b209f2f80d252c0b800b8
ibkDdon <ibukunoluwa_aina@yahoo.com>
2022-07-13 01:18:47 +0000

    ghjkjk

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Description
+ * Return: return value
+ */
+int main(void)
+{
+	char str1[26] = "abcdefghijklmnopqrstuvwxyz";
+	char str2[26] = "ABCDEFGHIJKLMNOPQRSTUVWXTZ";
+	int i = 0;
+	int n = 26;
+
+	for (i = 0; i < n; ++i)
+		putchar(str1[i]);
+
+	for (i = 0; i < n; ++i)
+		putchar(str2[i]);
+
+	putchar('\n');
+	return (0);
+}

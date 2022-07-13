fbb7a180899c9355ef8777eec8700cb034972196
ibkDdon <ibukunoluwa_aina@yahoo.com>
2022-07-13 01:32:13 +0000

    ghjkjk

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Description
+ * Return: return value
+ */
+int main(void)
+{
+	int i = 0;
+	int n = 10;
+
+	for (i = 0; i < n; ++i)
+		printf("%i", i);
+
+	printf('\n');
+	return (0);
+}

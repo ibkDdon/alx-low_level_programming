1e2ff458a06520eae75ceaab6eb795bd3f48387a
ibkDdon <ibukunoluwa_aina@yahoo.com>
2022-07-13 02:01:06 +0000

    ghjkjk

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Description
+ * Return: return value
+ */
+int main(void)
+{
+	char str1[26] = "zyxwvutsrqponmlkjihgfedcba";
+	int i = 0;
+	int n = 26;
+
+	for (i = 0; i < n; ++i)
+		putchar(str1[i]);
+
+	putchar('\n');
+	return (0);
+}

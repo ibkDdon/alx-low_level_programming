126da0c445c0f6fbfd6e86bc3c63cf24c41c14fa
ibkDdon <ibukunoluwa_aina@yahoo.com>
2022-07-13 01:02:21 +0000

    ghjkjk

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Description
+ * Return: return value
+ */
+int main(void)
+{
+	char str1[] = "abcdefghijklmnopqrstuvwxyz";
+
+	putchar(str1);
+	putchar('\n');
+	return (0);
+}

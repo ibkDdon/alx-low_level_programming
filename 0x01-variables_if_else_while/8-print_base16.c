b4f4890874a3181a23fcaa3e44b71675d70e6b16
ibkDdon <ibukunoluwa_aina@yahoo.com>
2022-07-14 05:32:04 +0000

    gdhsj

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- * main - Description
- * Return: return value
+ *  *main -> assign a random number to the variable n each time it is executed
+ *   *and print the last digit of the number stored in the variable n
+ *    *Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i = 0x0;
-	int n = 0x10;
+	char ch;
+	int n;
 
-	for (i = 0x0; i < n; ++i)
-		putchar(i + '0');
+	for (n = 48; n <= 57; n++)
+	{
 
-	putchar('\n');
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+
+		putchar(ch);
+	}
+
+	putchar(10);
+
 	return (0);
 }

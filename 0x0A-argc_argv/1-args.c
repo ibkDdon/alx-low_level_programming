bd6672cb86d84d0cce830fcec290e6076f0f49b6
ibkDdon <ibukunoluwa_aina@yahoo.com>
2022-07-24 02:57:29 +0000

    dhhj

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,11 +5,10 @@
  * *@argc: number of arguments
  * *@argv:array of arguments
  * *Return: the number of arguments
- * */
+ */
 
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-		printf("%d\n", argc - 1);
-
-			return (0);
+	printf("%d\n", argc - 1);
+	return (0);
 }

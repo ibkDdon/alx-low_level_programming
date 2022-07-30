ee33cd3b8c235aa05532b31c430a77f5863cce92
ibkDdon <ibukunoluwa_aina@yahoo.com>
2022-07-30 04:06:20 +0000

    ghjjkk

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -59,7 +59,25 @@ char *_initialize_array(char *ar, int lar)
  *    * @argv: arguments vector.
  *     * @n: row of the array.
  *      *
- *       * Return: length of the number.
+ *       * Return: length of the number.a++, k = lnout - (1 + ca);
+ *       }
+ *       if (j < 0)
+ *       {
+ *       if (nout[0] != '0')
+ *       break;
+ *       lnout--;
+ *       free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+ *       k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+ *       }
+ *       if (j >= 0)
+ *       {
+ *       add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+ *       addl = add / 10, nout[k] = (add % 10) + '0';
+ *       }
+ *       }
+ *       printf("%s\n", nout);
+ *       return (0);
+ *       }
  */
 int _checknum(char *argv[], int n)
 {
@@ -108,22 +126,4 @@ int main(int argc, char *argv[])
 					nout[k - 1] = (add / 10) + '0';
 				nout[k] = (add % 10) + '0';
 			}
-			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
-		}
-		if (j < 0)
-		{
-			if (nout[0] != '0')
-				break;
-			lnout--;
-			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-		}
-		if (j >= 0)
-		{
-			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
-			addl = add / 10, nout[k] = (add % 10) + '0';
-		}
-	}
-	printf("%s\n", nout);
-	return (0);
-}
+			i = ln1 - 1, j--, addl = 0, c

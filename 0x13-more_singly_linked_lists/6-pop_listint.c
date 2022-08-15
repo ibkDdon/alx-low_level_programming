e28a96a3cb55b05f8cc96d2110d8259c8a2b77fa
ibkDdon <ibukunoluwa_aina@yahoo.com>
2022-08-15 18:59:35 +0000

    more linked lists

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- *  *pop_listint - deletes the head node of a linked list
- *   *@head: head of the list
- *    *
- *     *Return: the deleted node data
+ * *pop_listint - deletes the head node of a linked list
+ * *@head: head of the list
+ * *
+ * *Return: the deleted node data
  */
 
 int pop_listint(listint_t **head)
@@ -21,4 +21,5 @@ int pop_listint(listint_t **head)
 
 	*head = (*head)->next;
 	return (content);
+
 }
